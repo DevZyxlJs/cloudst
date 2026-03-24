421e7366d1819a5f93fd20814f69a77bad75691b
ɪ'ᴍ ᴅɪᴇɢᴏ ᜊ•ᴗ•ᜊ <sarmientodieg@gmail.com>
2026-03-23 20:23:42 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+"use strict";
+
 const { wiki } = require('./cloud/wiki')
 const { mediafiredl, fbDownloader, ytplay } = require('./cloud/dl')
+const { pinterestSearch, tiktokSearch } = require('./cloud/searchs')
 
-module.exports = { wiki, mediafiredl, fbDownloader, ytplay
+module.exports = {
+  wiki,
+  mediafiredl,
+  fbDownloader,
+  ytplay,
+  tiktokSearch,
+  pinterestSearch
 }
